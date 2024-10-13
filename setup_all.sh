36bba7e9923e13255307c1eaa03b0adafa3fd0a9
SzymonOzog <szymon.ozog@gmail.com>
2024-10-13 08:53:58 +0200

    Update setup_all script

--- setup_all.sh
+++ setup_all.sh
@@ -1,4 +1,8 @@
-apt install i3 tmux
+apt install i3 tmux clang feh compton pasystray
 wget https://github.com/neovim/neovim/releases/download/v0.10.0/nvim.appimage
 chmod u+x nvim.appimage 
 mv ./nvim.appimage /usr/bin/nvim
+git clone https://github.com/SzymonOzog/nvim-config.git ~/.config/nvim
+git clone https://github.com/SzymonOzog/Wallpapers.git ~/Pictures/Wallpapers
+
+
